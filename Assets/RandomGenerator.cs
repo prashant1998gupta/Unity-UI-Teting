71bdc025281dff8a8a3b82b84a7810ca5beace6c
Prashant <prashant1998gupta@gamil.com>
2020-09-09 18:10:02 +0530

    Login and Registration UI part completed

---
+++ Assets/RandomGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class RandomGenerator : MonoBehaviour
+{
+    public float[] randomList;
+
+    void Start()
+    {
+        randomList = new float[1000000];
+        Thread t = new Thread(delegate ()
+        {
+            while (true)
+            {
+                Generate();
+                Thread.Sleep(16); // trigger the loop to run roughly every 60th of a second
+            }
+        });
+        t.Start();
+    }
+
+    void Generate()
+    {
+        System.Random rnd = new System.Random();
+        for (int i = 0; i < randomList.Length; i++)
+        {
+            randomList[i] = (float)rnd.NextDouble();
+            Debug.Log(randomList[i]);
+        }
+    }
+}
